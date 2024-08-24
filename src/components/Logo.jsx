df3284e41758f5164f9d17e5056bde4af562afbd
supriyaa05 <supriyaa0501@gmail.com>
2024-08-24 12:33:25 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({ width = '100px' }) {
+  return (
+    <div><img className='w-10 h-10 rounded-sm' src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724371200&semt=ais_hybrid" alt="" /></div>
+  )
+}
+
+export default Logo
